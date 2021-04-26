1b0fa8c29e582adf580fa3a9c25fbd590bf8dbb9
nadelacruz <nicoangelodelacruz@gmail.com>
2021-04-26 19:59:36 +0800

    Added main method.

---
+++ GeneralTester.java
@@ -0,0 +1,10 @@
+public class GeneralTester {
+    public static void main(String[] args) {
+        MainDatabase DB = new MainDatabase("Database.xlsx");
+        DB.go();
+        DB.go();
+        DB.go();
+        DB.go();
+        DB.go();
+    }
+}
