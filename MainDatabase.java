d50ebdaba44b93cb6ffc6946301892bde45566da
nadelacruz <nicoangelodelacruz@gmail.com>
2021-04-26 19:59:18 +0800

    Updated constructor and added dummy test method.

--- MainDatabase.java
+++ MainDatabase.java
@@ -3,10 +3,21 @@ import java.util.ArrayList;
 /** The main database class that contains all the Item objects. */
 public class MainDatabase {
     /** The arraylist object that contains all the item objects. */
-    ArrayList<Item> database;
+    public ArrayList<Item> database = null;
 
-    public MainDatabase() {
-        database = new ArrayList<Item>();
-
+    public MainDatabase(String fileLocationOfExcelFile) {
+        try {
+            database = ExcelFileToArrayList.convert(fileLocationOfExcelFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
+
+    /** Dummy method. Showcases choose method. */
+    public void go() {
+        Item temporaryItem = RandomizedItemChooser.choose(this.database, "Graph Theory: A Review", "Hard");
+        System.out.println(temporaryItem.getTopic() + "\n" + temporaryItem.getQuestion() + "\n" +
+                temporaryItem.getCorrectChoice());
+    }
+
 }
