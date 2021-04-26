d401fafb60a9fa94e284f61cfe5ee1078931aaad
nadelacruz <nicoangelodelacruz@gmail.com>
2021-04-26 15:36:07 +0800

    Added ExcelFileToArrayList class and added skeleton for its constructor.

---
+++ ExcelFileToArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/** Converts a given excel file to an arraylist of item objects. */
+public class ExcelFileToArrayList {
+    public static ArrayList<Item> convert(ArrayList<Item> temp, String fileLocationOfExcelFile) {
+        temp = new ArrayList<Item>();
+
+        
+
+        return temp;
+    }
+}
