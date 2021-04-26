edb5d83120a85148d63aac4a164ff05365eff9d7
nadelacruz <nicoangelodelacruz@gmail.com>
2021-04-26 15:34:23 +0800

    Added MainDatabase class and added skeleton for its constructor.

---
+++ MainDatabase.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/** The main database class that contains all the Item objects. */
+public class MainDatabase {
+    /** The arraylist object that contains all the item objects. */
+    ArrayList<Item> database;
+
+    public MainDatabase() {
+        database = new ArrayList<Item>();
+
+    }
+}
