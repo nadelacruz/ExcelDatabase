8c3ee14a0aaefe723ece88e987afc93152565db9
nadelacruz <nicoangelodelacruz@gmail.com>
2021-04-26 19:10:47 +0800

    Added IndexFetcher class and its following fetch methods.

---
+++ IndexFetcher.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/** Fetches the indexes of the item objects with the specified parameters. */
+public class IndexFetcher {
+    /** Fetches the indexes with the specified topic and difficulty. */
+    public static ArrayList<Integer> fetch(ArrayList<Item> database, String topic, String difficulty) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < database.size(); i++) {
+            if (database.get(i).getTopic().equals(topic) && database.get(i).getDifficulty().equals(difficulty)) {
+                temp.add(i);
+            }
+        }
+        return temp;
+    }
+
+    /** Fetches the indexes with the specified difficulty. */
+    public static ArrayList<Integer> fetch(ArrayList<Item> database, String difficulty) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < database.size(); i++) {
+            if (database.get(i).getDifficulty().equals(difficulty)) {
+                temp.add(i);
+            }
+        }
+        return temp;
+    }
+}
